0045d41fc807964b89d770df06ea5ac5ad9eeb02
Niren Orie <nireno@gmail.com>
2019-07-03 16:51:47 -0400

    Add script to verify .env contains all keys in .env.template

---
+++ scripts/dotenv_diff.js
@@ -0,0 +1,23 @@
+let {green, red, yellow} = require("ansi-colors");
+let path = require('path');
+let fs = require('fs');
+let dotenv = require('dotenv');
+
+let env = fs.readFileSync(path.resolve(process.cwd(), '.env'))
+let envTemplate = fs.readFileSync(path.resolve(process.cwd(), '.env.template'))
+
+let envConfig = dotenv.parse(env);
+let envTemplateConfig = dotenv.parse(envTemplate);
+
+let envKeys = Object.keys(envConfig);
+let envTemplateKeys = Object.keys(envTemplateConfig);
+
+let missingKeys = envTemplateKeys.filter(key => !envKeys.some(envKey => envKey == key));
+// ensure all keys are defined
+if(envKeys.join() !== envTemplateKeys.join()){
+  console.error(red(".env and .env.template are out of sync "));
+  // console.error(green("expected: "), envTemplateKeys)
+  // console.error(red("got: "), envKeys)
+  console.error("Add the following variables to your .env file: ", missingKeys);
+  process.exit(1);
+}
