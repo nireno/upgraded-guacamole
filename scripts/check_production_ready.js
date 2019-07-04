6255fda41e8877850eb9bcc10a98a02a07055dee
Niren Orie <nireno@gmail.com>
2019-07-04 09:17:51 -0400

    Modify update script

--- scripts/check_production_ready.js
+++ scripts/check_production_ready.js
@@ -1,7 +1,13 @@
-let {red, yellow} = require("ansi-colors");
+let { red, yellow } = require("ansi-colors");
 let path = require('path');
-let fs = require('fs');
+let fs = require('fs-extra');
 let dotenv = require('dotenv');
+
+// Ensure .env exists
+if (!(fs.existsSync(path.resolve(process.cwd(), ".env")))) {
+  console.error(red("`.env` file required. See `.env.template`."));
+  process.exit(1);
+};
 
 let env = fs.readFileSync(path.resolve(process.cwd(), '.env'))
 let envTemplate = fs.readFileSync(path.resolve(process.cwd(), '.env.template'))
@@ -12,10 +18,17 @@ let envTemplateConfig = dotenv.parse(envTemplate);
 let envKeys = Object.keys(envConfig);
 let envTemplateKeys = Object.keys(envTemplateConfig);
 
+// Ensure all environment variables are defined
 let missingKeys = envTemplateKeys.filter(key => !envKeys.some(envKey => envKey == key));
-// ensure all keys are defined
-if(envKeys.join() !== envTemplateKeys.join()){
+if (envKeys.join() !== envTemplateKeys.join()) {
   console.error(red(".env and .env.template are out of sync "));
   console.error(yellow("Add the following variables to your .env file: "), missingKeys);
   process.exit(1);
 }
+
+// Ensure NODE_ENV=production
+dotenv.config();
+if (!(process.env.NODE_ENV == "production")) {
+  console.error(red("Expected NODE_ENV=production but got NODE_ENV=" + process.env.NODE_ENV));
+  process.exit(1);
+};
