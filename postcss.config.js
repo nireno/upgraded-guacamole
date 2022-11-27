876cfa2d18cc2802465534980ffe605877c52942
Niren Orie <nireno@gmail.com>
2022-11-27 10:23:22 -0400

    Upgrade to tailwind 3

--- postcss.config.js
+++ postcss.config.js
@@ -1,29 +1,14 @@
-require('dotenv').config();
-
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  // Specify the paths to all of the template files in your project purgecss
-  // will scan the following "content" files to determine which styles need to
-  // be preserved. See
-  // https://tailwindcss.com/docs/controlling-file-size/#writing-purgeable-html
-  // for recommendations on writing purgeable tailwind classes.
-  content: [
-    './src/**/*.re',
-    './src/**/*.html',
-    // './src/**/*.jsx',
-    // etc.
-  ],
-
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-})
+let productionPlugins =
+  process.env.NODE_ENV === "production"
+    ? {
+        cssnano: {},
+      }
+    : {};
 
 module.exports = {
-  plugins: [
-    require('precss'),
-    require('tailwindcss')('./tailwind.js'),
-    require('autoprefixer'),
-    ...process.env.NODE_ENV === 'production'
-    ? [purgecss, require('cssnano')]
-    : []
-  ],
-}
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...productionPlugins,
+  },
+};
